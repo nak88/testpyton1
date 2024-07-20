863c7875453228aacff1f02d9f8215efe1c8d110
nak88 <channakpheap210704@gmail.com>
2024-07-20 10:16:45 +0700

    first

---
+++ app.py
@@ -0,0 +1,15 @@
+print("Hello Nak")
+print("HI")
+
+print("Single 'quot' inside double quote")
+print('Double')
+
+
+print("Wellcome Amount call")
+qty1=int(input("Qty1 ="))
+qty2=int(input("Qty2 ="))
+price=float(input("Price ="))
+total_qty=qty1+qty2
+print("Total Qty= "+str(total_qty))
+amount=total_qty*price
+print("Total Amount= "+str(amount))
